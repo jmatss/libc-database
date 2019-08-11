3b679c365c3e5a0bd41541f526434957cb61eaf2
Jonathan Matsson <jmatss@kth.se>
2019-08-11 14:36:09 -0400

    Added find_regex.py that lets one search for matches with regex

---
+++ find_regex.py
@@ -0,0 +1,40 @@
+from sys import argv
+import os, re
+
+def usage():
+	print("%-7s %s %s" % ("usage:", argv[0], "<regex (on the symbol files)> ..."))
+	exit()
+
+def find_matches(arguments):
+	results = []
+	root_dir = "db" if len(os.path.dirname(argv[0])) == 0 else os.path.dirname(argv[0]) + "/db"
+
+	# Turn arguments into regex
+	patterns = []
+	for argument in arguments:
+		patterns.append(re.compile(argument))
+
+	# Loop through all files and add files that match ALL arguments to results
+	for filename in os.listdir(root_dir):
+		if filename.endswith(".symbols"):
+			file_results = []
+			file_patterns_matched = [False] * len(arguments)
+
+			for _, line in enumerate(open(root_dir + "/" + filename)):
+				for i, pattern in enumerate(patterns):
+					if pattern.search(line):
+						file_patterns_matched[i] = True
+						file_results.append(line)
+
+			if False not in file_patterns_matched:
+				results.append((filename, file_results))
+
+	return results
+
+if len(argv) < 2:
+	usage()
+
+for result in find_matches(argv[1:]):
+	print(result[0])
+	for line in result[1]:
+		print("  " + line.strip())
